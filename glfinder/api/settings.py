7be4b3116a58ad7a27d21c49785831a3f850f48f
Grishon Nganga <grishon.nganga01@gmail.com>
2021-12-26 22:31:20 +0300

    Remove old glfinder dir

--- glfinder/api/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DB_HOST = os.environ.get('POSTGRES_HOSTNAME')
-DB_PORT = os.environ.get('POSTGRES_PORT')
-DB_USER = os.environ.get('POSTGRES_USERNAME')
-DB_PASS = os.environ.get('POSTGRES_PASSWORD')
-DB_NAME = os.environ.get('POSTGRES_DB')
-DB_CONN = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
